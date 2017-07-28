2200438d388228cb7c0569dc40938d1f35ab1073
J Fixby <github@jfixby.com>
2017-07-28 11:26:47 +0200

    compile against flutter api

--- flutter-api/src/io/flutter/plugin/common/MessageCodec.java
+++
@@ -1,9 +0,0 @@
-package io.flutter.plugin.common;
-
-import java.nio.ByteBuffer;
-
-public interface MessageCodec {
-   ByteBuffer encodeMessage(Object var1);
-
-   Object decodeMessage(ByteBuffer var1);
-}
